a544136ba185ae99f1372b58d15c29228e1de30b
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-09-17 13:20:48 -0300

    Login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {ButtonModule} from 'primeng-lts/button';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+		ButtonModule
   ],
 	exports:[HomeComponent]
 })
