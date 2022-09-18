6c8db4894c11a4b706290021e513a27fadcbbb92
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-09-18 20:56:15 -0300

    Valicacao customizada

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { minusculoValidator } from './minusculo-validator';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(private formBuilder:FormBuilder) {
 		this.novoUsuarioForm = this.formBuilder.group({
 
-			userName:['',[Validators.required,Validators.minLength(5),Validators.maxLength(120)]],
+			userName:['',[Validators.required,minusculoValidator]],
 			fullName:['',[Validators.required,Validators.minLength(5),Validators.maxLength(120)]],
 			email:['',[Validators.required,Validators.email]]
 
