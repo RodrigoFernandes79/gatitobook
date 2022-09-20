5ee29915ad8b1270972f85d30825a029ef5e806b
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-09-19 21:57:20 -0300

    npm i jwt-decode (TOKEN)2

--- src/app/autenticacao/token.service.ts
+++ src/app/autenticacao/token.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+const KEY = 'token'
+
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
   constructor() { }
+
+	retornaToken() {
+		return localStorage.getItem(KEY) ?? ''
+	}
+
+	salvaToken(token: string) {
+		return localStorage.setItem(KEY, token)
+	}
+
+	excluiToken() {
+	localStorage.removeItem(KEY)
+	}
+
+	possuiToken() {
+		return !!this.retornaToken() //retorna um boleano : verifica se tem ou nao tem token
+	}
+
 }
